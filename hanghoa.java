9ed3d889028d7db0b2789cfc2ec8ccfc9026ef8d
Namj15 <125299505+Namj15@users.noreply.github.com>
2023-05-15 17:03:38 +0700

    Create hanghoa.java

---
+++ hanghoa.java
@@ -0,0 +1,20 @@
+Chat GPT làm 
+package bai2.newpackage;
+
+abstract class HangHoa {
+    protected String maHang;
+    protected String tenHang;
+    protected int soLuongTon;
+    protected double donGia;
+
+    public HangHoa(String maHang, String tenHang, int soLuongTon, double donGia) {
+        this.maHang = maHang;
+        this.tenHang = tenHang;
+        this.soLuongTon = soLuongTon;
+        this.donGia = donGia;
+    }
+
+    public abstract double tinhTienVAT();
+
+    public abstract void danhGia();
+}
